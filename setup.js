38fc2eb805f0300ede1e375aa532ba963a5f385f
zanisis <adzannyaa@gmail.com>
2017-04-10 18:04:01 +0700

    adzanny done

--- setup.js
+++ setup.js
@@ -1,3 +1,8 @@
 "use strict"
+const repl = require('repl');
+const sqlite3 = require('sqlite3').verbose();
 
+let file = 'student.db'
+var db = new sqlite.Database(file)
 //write your code here
+//SQL 
