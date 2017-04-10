ad667e599d3600ba1cb1e6b81b43ca27b4b3ffd9
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-04-10 15:35:43 +0700

    selesai

--- setup.js
+++ setup.js
@@ -1,3 +1,41 @@
 "use strict"
 
 //write your code here
+const repl = require('repl');
+const sqlite = require('sqlite3').verbose();
+
+// var file = 'student.db';
+// var db = new sqlite.Database('./student.db');
+var db = new sqlite.Database('./student.db');
+
+let replServer = repl.start({prompt : '> '});
+
+var CREATE_TABLE = "CREATE TABLE IF NOT EXISTS student (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT, birthdate TEXT)";
+var SEED_DATA = "INSERT INTO student (firstname, lastname, birthdate) VALUES ('Rubi', 'Henjaya', '1986-11-20'), ('Riza', 'Fahmi', '1983-12-31');"
+
+let createTable = () => {
+  db.serialize(function(){
+    db.run(CREATE_TABLE, function(err){
+      if (err){
+        console.log(err);
+      } else {
+        console.log(CREATE TABLE);
+      }
+    })
+  });
+}
+
+let seedData = () => {
+  db.serialize(function(){
+    db.run(SEED_DATA, function(err){
+      if (err){
+        console.log(err);
+      } else {
+        console.log(SEED DATA);
+      }
+    })
+  });
+}
+
+replServer.context.createTable = createTable;
+replServer.context.seedData = seedData;
