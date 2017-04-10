df4358757927ac05a84a9fd10689ed5f0a9ed0fd
Isumi & Eri Irawan <pair+isumizumi.mrei91@hacktiv8.com>
2017-04-10 10:41:41 +0700

    create student class

--- student_roster.js
+++ student_roster.js
@@ -4,3 +4,30 @@ const repl = require('repl');
 const sqlite = require('sqlite3').verbose();
 
 // write your code here
+class Student {
+  constructor () {
+
+  }
+
+  addStudent(firstname, lastname, birthdate) {
+    let query = `INSERT INTO Students(firstname, lastname, birthdate) VALUES ('${firstname}', '${lastname}', '${birthdate}')`;
+
+  }
+
+
+  runQuery(query) {
+    db.serialize(function () {
+      db.run(query, function (err) {
+        if (err) {
+          console.log(err);
+        }else {
+          console.log('QUERY Ran Successfully!');
+        }
+      });
+    });
+
+  }
+
+
+
+}
