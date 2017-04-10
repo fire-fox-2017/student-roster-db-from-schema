a28b0608c548e4b10f13833741da9dc4a08dff0f
Bhirawa Mbani <bhirmbani@gmail.com>
2017-04-10 14:56:36 +0700

    initial commit first done student db

--- setup.js
+++ setup.js
@@ -1,3 +1,46 @@
 "use strict"
 
 //write your code here
+const repl = require('repl');
+const sqlite = require('sqlite3').verbose();
+
+var file = 'student.db';
+var db = new sqlite.Database(file);
+
+
+// SQL Statement
+var CREATE_TABLE = "CREATE TABLE IF NOT EXISTS students ( id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT, birthdate DATE );";
+var SEED_DATA = "INSERT INTO students (firstname, lastname, birthdate) VALUES ('Rubi', 'Henjaya', '1986-11-20'), ('Riza', 'Fahmi', '1983-12-31');";
+
+// CREATE_TABLE
+let createTable = () => {
+  // Run SQL one at a time
+  db.serialize(function() {
+    // create table
+    db.run(CREATE_TABLE, function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log('Table students created');
+      }
+    });
+  });
+}
+
+// SEED_DATA
+let seedData = () => {
+  db.serialize(function() {
+    db.run(SEED_DATA, function(err) {
+      if(err) { 
+        console.log(err);
+      } else {
+        console.log('successfully seed data to table students');
+      }
+    });
+  });
+}
+
+var replServer = repl.start('> ');
+
+replServer.context.createtable = createTable;
+replServer.context.seeddata = seedData;
