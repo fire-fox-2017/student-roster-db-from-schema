ee61d76a58aa5fd636e11e74b82a116aa93f506d
LeCodeBae <billivank_96@yahoo.co.id>
2017-04-10 18:22:45 +0700

    beres

--- setup.js
+++ setup.js
@@ -1,3 +1,44 @@
 "use strict"
 
-//write your code here
+const repl = require('repl');
+const sqlite = require('sqlite3').verbose();
+
+var file = 'student.db';
+var db = new sqlite.Database(file);
+
+// SQL Statement
+var CREATE_TABLE = "CREATE TABLE IF NOT EXISTS student ( id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT, birthdate DATE);";
+var SEED_DATA = "INSERT INTO student (firstname, lastname, birthdate) VALUES ('Rubi', 'Henjaya', '1986-11-20'), ('Riza', 'Fahmi', '1983-12-31');";
+
+// CREATE_TABLE
+let createTable = () => {
+  // Run SQL one at a time
+  db.serialize(function() {
+    // Create table
+    db.run(CREATE_TABLE, function(err) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        console.log('CREATE TABLE');
+      }
+    });
+  });
+}
+
+// SEED_DATA
+let seedData = () => {
+  // Run SQL one at a time
+  db.serialize(function() {
+    // Create table
+    db.run(SEED_DATA, function(err) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        console.log('SEED DATA');
+      }
+    });
+  });
+}
+let rp = repl.start('> ');
+rp.context.createTable = createTable;
+rp.context.seedData = seedData;
--- student_roster.js
+++ student_roster.js
@@ -2,5 +2,107 @@
 
 const repl = require('repl');
 const sqlite = require('sqlite3').verbose();
+var file = 'student.db';
+var db = new sqlite.Database(file);
 
-// write your code here
+class Student {
+  static addStudent(firstname, lastname, birthdate) {
+    db.serialize(function() {
+     db.run(`INSERT INTO student (firstname, lastname, birthdate) VALUES ('${firstname}', '${lastname}', '${birthdate}');`, function(err) {
+       if (err) {
+         console.log(err.message);
+       } else {
+         console.log('STUDENT ADDED');
+       }
+     });
+   });
+ }
+ static updateStudent(id,attribute,value) {
+   db.serialize(function() {
+     db.run(`UPDATE student SET ${attribute} = '${value}' WHERE id = ${id};`, function(err) {
+       if (err) {
+         console.log(err.message);
+       } else {
+         console.log('STUDENT UPDATED');
+       }
+     });
+   });
+ }
+ static deleteStudent(id) {
+   db.serialize(function() {
+     db.run(`DELETE FROM student WHERE id = ${id};`, function(err) {
+       if (err) {
+         console.log(err.message);
+       } else {
+         console.log('STUDENT DELETED');
+       }
+     });
+   });
+ }
+ static listStudents() {
+    db.each(`SELECT * FROM student;`, function(err,data) {
+       if (err) {
+         console.log(err.message);
+       } else {
+         console.log(data);
+       }
+    });
+ }
+ static searchStudentName(name) {
+   db.each(`SELECT * FROM student WHERE firstname = '${name}' OR lastname = '${name}';`, function(err,data) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        console.log(data);
+      }
+   });
+ }
+ static searchStudent(attribute,value) {
+   db.each(`SELECT * FROM student WHERE ${attribute} = '${value}';`, function(err,data) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        console.log(data);
+      }
+   });
+ }
+ static listBirthMonth() {
+   db.each(`SELECT * FROM student WHERE strftime('%m', birthdate) = strftime('%m', 'now');`,function(err, data){
+     if(err){
+       console.log(err.message)
+     } else {
+       console.log(data)
+     }
+   })
+   
+ }
+ static listBirthday() {
+   db.each(`SELECT firstname, lastname, strftime('%m', birthdate) as Month, strftime('%d', birthdate) as Day FROM student ORDER BY strftime('%m''%d',birthdate);`,function(err,data){
+     if(err){
+       console.log(err.message)
+     } else {
+       console.log(data)
+     }
+   })
+ }
+ static help() {
+   console.log('addStudent(firstname, lastname, birthdate)');
+   console.log('updateStudent(id,attribute,value)');
+   console.log('deleteStudent(id)');
+   console.log('listStudents()');
+   console.log('searchStudentName(name)');
+   console.log('searchStudent(attribute,name)');
+   console.log('listBirthMonth()');
+   console.log('listBirthday()')
+ }
+}
+let rp=repl.start('>>> ')
+rp.context.addStudent = Student.addStudent
+rp.context.updateStudent = Student.updateStudent
+rp.context.deleteStudent = Student.deleteStudent
+rp.context.listStudents = Student.listStudents
+rp.context.searchStudentName = Student.searchStudentName
+rp.context.searchStudent = Student.searchStudent
+rp.context.listBirthMonth = Student.listBirthMonth
+rp.context.listBirthday = Student.listBirthday
+rp.context.help = Student.help
