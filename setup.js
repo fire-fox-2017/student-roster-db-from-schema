377292a33def9b124b9c536a62f734d4633a0ccb
jamessraun <jeezybig@gmail.com>
2017-04-17 15:32:51 +0700

    first commit

--- setup.js
+++ setup.js
@@ -1,3 +1,45 @@
 "use strict"
 
-//write your code here
+const repl = require('repl');
+const sqlite = require('sqlite3').verbose();
+
+// write your code here
+var file = 'student.db';
+var db = new sqlite.Database(file);
+
+const r=repl.start('$ ');
+
+var CREATE_TABLE = "CREATE TABLE IF NOT EXISTS student (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT, birthdate DATE)";
+var SEED_DATA = "INSERT INTO student (firstname,lastname,birthdate) VALUES ('Rubi','Henjaya','1986-11-20'),('Riza','Fahmi','1983-12-31');";
+
+// CREATE_TABLE
+let createTable = () => {
+  db.serialize(function(){
+    //create table
+    db.run(CREATE_TABLE, function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log('CREATE TABLE');
+      }
+    });
+  });
+}
+
+//SEED_DATA
+let seedData = () => {
+//Your code here
+db.serialize(function(){
+  //create table
+  db.run(SEED_DATA, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log('SEED TABLE');
+    }
+  });
+});
+}
+
+r.context.createTable = createTable;
+r.context.seedData = seedData;
